217deeb17238d424fccd6a306cc9f8226435b8d0
Colin Scally <colin.scally@gmail.com>
2017-05-24 14:36:22 +0100

    Add form to input player names, add post redirect loop which stores inputted names in a session then redirects to another path to view

--- app.rb
+++ app.rb
@@ -2,9 +2,24 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-get '/' do
-  "Welcome to the Thunderdome!\nTesting Infrastructure Working!"
-end
+  run! if app_file == $0
 
-run! if app_file==$0
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name1 = session[:player_1_name]
+    @name2 = session[:player_2_name]
+    erb :play
+  end
+
 end
---
+++ spec/features/testing_name_entry_spec.rb
@@ -0,0 +1,9 @@
+feature 'testing name entry and submission' do
+  scenario 'players can enter their names' do
+    visit('/')
+    fill_in('player_1_name', with: "Player 1")
+    fill_in('player_2_name', with: "Player 2")
+    click_button 'Submit'
+    expect(page).to have_content("Player 1 vs Player 2")
+  end
+end
